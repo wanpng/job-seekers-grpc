5573b19fa2b69195a3c1be145121cda2f7cc4397
Raymond Sarmiento <raymondsarmiento@Raymonds-MacBook-Pro.local>
2021-03-12 14:49:28 +0800

    Update employment type

--- build/protoc-gen.sh
+++ build/protoc-gen.sh
@@ -1,5 +1,6 @@
 protoc --go_out=$GOPATH/src protos/domain/*.proto
 protoc-go-inject-tag -input=grpc/domain/jobseeker.pb.go
+protoc-go-inject-tag -input=grpc/domain/employment_type.pb.go
 protoc-go-inject-tag -input=grpc/domain/skill.pb.go
 protoc-go-inject-tag -input=grpc/domain/education.pb.go
 protoc-go-inject-tag -input=grpc/domain/training.pb.go
